f73d3468fd2b8e1a736158f8df3c7e47a019c0a4
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-04-17 19:47:19 +0430

    second change

--- index.js
+++ index.js
@@ -6,8 +6,29 @@ const app = express()
 const PORT = 3000;
 
 // second argument is handler, once the route gets found, handle should take action
+
+// Have it in your mind that, get is something you can see on the UI
+// GET
 app.get("/", (request, response) => {
     response.send("This is a get request at /")
+})
+
+
+// POST
+app.post('/create', (request, response) => {
+    response.send("this is a post request from /create")
+})
+
+
+// PUT
+app.put('/edit', (request, response) => {
+    response.send("This is a put method at /edit")
+})
+
+
+// DELETE
+app.delete("/delete", (request, response) => {
+    response.send("This is a deleete request at /delete")
 })
 
 
@@ -15,3 +36,8 @@ app.listen(PORT, () => {
     console.log(`This app is running on port ${PORT}`)
     console.log(data)
 })
+
+
+
+// Have it in your mind that , by response we send a message to the client
+// Go to Post man to test your HttpRequest
